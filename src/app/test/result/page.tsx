f0fc344c0df912e722a8ba87c1e3e02be04c9ed9
Kim Seo-Yul <yul_the@naver.com>
2025-04-21 16:10:37 +0900

    feat : 결과 UI 수정, 테스트 결과 수정

--- src/app/test/result/page.tsx
+++ src/app/test/result/page.tsx
@@ -5,21 +5,30 @@ import Mercury from '@/images/수성.png';
 const mockData = {
   id: 'mercury',
   title: '🧭 흐름을 좇는 감성 방랑자',
-  mbti: ['ENFP', 'ENTP'],
-  description:
-    '즉흥적인 감각과 낭만을 가진 당신은 자유로운 흐름을 즐기는 탐험가예요. 밤공기, 조명, 분위기… 그날의 기분이 길을 정하죠.',
-  features: ['즉흥적인 경로 선택', '분위기에 민감', '우연한 발견을 즐김'],
-  keywords: ['강변 산책로', '조용한 골목길', '우연히 만나는 뷰'],
+  summary: '기분 따라 걷는 감성적 자유인',
+  features: [
+    '기분 따라 발길 닿는 대로 걷는 걸 좋아해요.',
+    '우연히 발견한 길이나 풍경에서 감동을 받아요.',
+    '계획보다 분위기와 순간을 중시해요.',
+  ],
 };
 
 export default function Page() {
   return (
-    <div>
-      <div>{mockData.title}</div>
-      <Image src={Mercury} width={100} height={100} alt={mockData.id} />
-      <div>{mockData.description}</div>
-
-      <div className="">
+    <div className="flex flex-col gap-10">
+      {/* <h1 className="text-4xl font-bold">{mockData.type}</h1> */}
+      <div className="text-3xl font-bold">{mockData.title}</div>
+      <Image src={Mercury} width={100} height={100} alt={mockData.id} className="w-sm m-auto" />
+      <section>
+        <div className="text-2xl font-bold">당신의 특징은?</div>
+        {mockData.features.map((feature) => (
+          <div className="text-lg">- {feature}</div>
+        ))}
+      </section>
+      <section>
+        <div className="text-2xl font-bold">당신에게 추천하는 야경 명소</div>
+      </section>
+      <div className="sticky">
         <Button>결과 공유하기</Button>
       </div>
     </div>
