a5579c7ef014341354e2caf08dbbdbc145748942
kimsyul <yul_the@naver.com>
2025-04-28 07:32:01 +0900

    feat : 개별 spot 조회

--- src/actions/spotAction.ts
+++ src/actions/spotAction.ts
@@ -9,11 +9,28 @@ function handleError(error: PostgrestError | null) {
   throw new Error(error?.message);
 }
 
-export async function getAllSpots() {
+export async function getAllSpots({ search, page, pageSize }: { search?: string; page: number; pageSize: number }) {
   const cookieStore = await cookies();
   const supabase = createClient(cookieStore);
 
-  const { data, error } = await supabase.from('spots').select('*');
+  const { data, error } = await supabase
+    .from('spots')
+    .select('*')
+    .like('title', `%${search}%`)
+    .range((page - 1) * pageSize, page * pageSize - 1);
+
+  if (error) {
+    handleError(error);
+  }
+
+  return data;
+}
+
+export async function getSpot(num: number) {
+  const cookieStore = await cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data, error } = await supabase.from('spots').select('*').eq('num', num).maybeSingle();
 
   if (error) {
     handleError(error);
--- src/components/SpotList.tsx
+++ src/components/SpotList.tsx
@@ -1,21 +1,27 @@
 'use client';
 
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import SpotCard from './SpotCard';
 import { getAllSpots } from '@/actions/spotAction';
 import { Skeleton } from './ui/skeleton';
+import { useInView } from 'react-intersection-observer';
 
-export default function SpotList() {
-  const { data, isPending } = useQuery({
-    queryKey: ['spots'],
-    queryFn: () => getAllSpots(),
-  });
-  console.log(data);
+export default function SpotList({ search }: { search: string }) {
+  // TODO
+  // const { data, isFetching, isFetchingNextPage, fetchNextPage, hasNextPage } = useInfiniteQuery({
+  //   initialPageParam: 1,
+  //   queryKey: ['spots', search],
+  //   queryFn: ({ pageParam }) => getAllSpots({ search, page: pageParam, pageSize: 5 }),
+  //   getNextPageParam: (lastPage) => (lastPage.page ? )
+  // });
+
+  const { ref, inView } = useInView({ threshold: 0 });
 
   return (
     <>
-      {isPending && <Skeleton className="rounded-2xl w-3/4 m-auto" />}
+      {(isFetching || isFetchingNextPage) && <Skeleton className="rounded-2xl w-3/4 m-auto bg-amber-200" />}
       {data && data.map((spot) => <SpotCard key={spot.num} {...spot} />)}
+      <div ref={ref}></div>
     </>
   );
 }
