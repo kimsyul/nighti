30f5c637d6a74af6c434b4d207d8172147e39d28
kimsyul <yul_the@naver.com>
2025-05-09 21:10:57 +0900

    fix : Suspense 추가

--- src/app/(logo-search-header)/layout.tsx
+++ src/app/(logo-search-header)/layout.tsx
@@ -1,10 +1,12 @@
 import LogoAndSearch from '@/components/LogoAndSearch';
-import { ReactNode } from 'react';
+import { ReactNode, Suspense } from 'react';
 
 export default function first({ children }: { children: ReactNode }) {
   return (
     <div className="max-w-lg h-screen overflow-y-auto bg-[url(/bg.png)] shadow-2xl shadow-amber-200 bg-center bg-cover flex flex-col pb-4 m-auto">
-      <LogoAndSearch />
+      <Suspense>
+        <LogoAndSearch />
+      </Suspense>
       <main className="flex-1 px-6 py-4">{children}</main>
       <footer className="text-center text-white text-sm my-3">Copyright © 2025 - All right reserved by KSY</footer>
     </div>
