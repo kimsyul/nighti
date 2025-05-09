0612fe01b592f4fe62a751980ed7c5c424231c5e
Kim Seo-Yul <yul_the@naver.com>
2025-05-09 16:07:48 +0900

    fix : 타입 에러 수정

--- src/app/(with-back-header)/spot/[id]/page.tsx
+++ src/app/(with-back-header)/spot/[id]/page.tsx
@@ -1,13 +1,7 @@
 import { getSpot } from '@/actions/spotAction';
 import { SpotDetail } from '@/components/SpotDetail';
 
-type PageProps = {
-  params: {
-    id: string;
-  };
-};
-
-export default async function Page({ params }: PageProps) {
+export default async function Page({ params }: { params: { id: string } }) {
   const id = Number(params.id);
   const spot = await getSpot(id);
 
